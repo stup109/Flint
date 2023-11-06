8637219d0b78288faab7ef60bcc6d24d2bb80799
stup109 <148747974+stup109@users.noreply.github.com>
2023-11-06 12:58:13 +0800

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -1,1 +1,5 @@
-
+cd /root/
+touch Flint_Dashboard
+cd Flint_Dashboard
+wget https://raw.githubusercontent.com/stup109/Flint/main/dashboard.js
+wget https://raw.githubusercontent.com/stup109/Flint/main/dashboard.ejs
