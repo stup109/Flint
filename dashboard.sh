3b3bb32f613422ce17a5c313dc0105068510d356
stup109 <148747974+stup109@users.noreply.github.com>
2023-11-06 13:13:29 +0800

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -1,5 +1,10 @@
 cd /root/
-touch Flint_Dashboard
-cd Flint_Dashboard
+mkdir F
+cd ./F
+
+npm init -y
+npm install express
+
 wget https://raw.githubusercontent.com/stup109/Flint/main/dashboard.js
 wget https://raw.githubusercontent.com/stup109/Flint/main/dashboard.ejs
+echo "Job Done?"
