7e11d81cf20d474303e12f2daa3c911eccb136ac
stup109 <148747974+stup109@users.noreply.github.com>
2023-12-03 23:32:19 +0800

    Add files via upload

--- App/index.py
+++ App/index.py
@@ -1,25 +1,26 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, redirect, url_for
 import os
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/change_hostname', methods=['POST'])
-def change_hostname():
-    new_hostname = request.form['new_hostname']
-    os.system(f'sudo hostname {new_hostname}')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    hostname = os.uname().nodename
 
-    with open('/etc/hosts', 'r') as file:
-        lines = file.readlines()
+    if request.method == 'POST':
+        new_hostname = request.form['hostname']
 
-    with open('/etc/hosts', 'w') as file:
-        for line in lines:
-            if line.startswith('127.0.1.1'):
-                file.write(f'127.0.1.1 {new_hostname}\n')
-            else:
-                file.write(line)
+        # Executing hostnamectl command
+        subprocess.run(["hostnamectl", "set-hostname", new_hostname])
 
-    return 'Hostname has been changed to ' + new_hostname
+        # Writing new hostname to the file
+        with open("/etc/hostname", "w") as f:
+            f.write(new_hostname)
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', hostname=hostname)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
