f148da9d706a16a846afd665a9aa1e8a3751b682
stup109 <148747974+stup109@users.noreply.github.com>
2023-12-04 01:03:48 +0800

    Update index.py

--- App/server/index.py
+++ App/server/index.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/update_hostname', methods=['POST'])
+def update_hostname():
+    if request.method == 'POST':
+        new_hostname = request.form['new_hostname']
+        subprocess.call(['hostnamectl', 'set-hostname', new_hostname])
+        return f'Hostname updated to {new_hostname}'
+    else:
+        return 'Only POST requests are allowed'
+
 
 if __name__ == '__main__':
     app.debug = True
